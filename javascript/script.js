cc4824ce8fde9ac16911ec17fa2ac5503e30c350
Doriano Benyocki <doriano.benyocki@avl.com>
2025-10-23 14:00:13 +0200

    Updated source paths in the script.js file

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const images = [
-        '../images/crash.png',
-        '../images/kick.png',
-        '../images/snare.png',
-        '../images/tom1.png',
-        '../images/tom2.png',
-        '../images/tom3.png',
-        '../images/tom4.png'
+        'images/crash.png',
+        'images/kick.png',
+        'images/snare.png',
+        'images/tom1.png',
+        'images/tom2.png',
+        'images/tom3.png',
+        'images/tom4.png'
     ];
 
     const sounds = [
-        '../sounds/crash.mp3',
-        '../sounds/kick-bass.mp3',
-        '../sounds/snare.mp3',
-        '../sounds/tom-1.mp3',
-        '../sounds/tom-2.mp3',
-        '../sounds/tom-3.mp3',
-        '../sounds/tom-4.mp3'
+        'sounds/crash.mp3',
+        'sounds/kick-bass.mp3',
+        'sounds/snare.mp3',
+        'sounds/tom-1.mp3',
+        'sounds/tom-2.mp3',
+        'sounds/tom-3.mp3',
+        'sounds/tom-4.mp3'
     ]
 
     const keyMap = {
